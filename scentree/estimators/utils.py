7a0c646de8f38340a223e11c0629a7a8ce10b7af
pachoning <cc.pachon@gmail.com>
2026-03-06 20:13:35 +0100

    Fix the trend to none on VAR model

--- scentree/estimators/utils.py
+++ scentree/estimators/utils.py
@@ -7,7 +7,7 @@ HYPERPARAMETERS_SPACE: Dict[str, Dict[str, List[Any]]] = {
         "alpha": [0.05, 0.06, 0.08, 0.1, 0.2, 0.25, 0.5, 0.7, 1, 2],
         "max_iter": [100, 200, 300, 500, 1000],
     },
-    "VAR": {"maxlags": [2, 5, 7, 10], "trend": ["n", "c", "ct", "ctt"]},
+    "VAR": {"maxlags": [2, 5, 7, 10], "trend": ["n"]},
 }
 
 
