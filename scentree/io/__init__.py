30009076f1b4231a9deb8c07b2ad5c3b80039234
pachoning <cc.pachon@gmail.com>
2026-04-22 14:06:32 +0200

    Add stages in dataset representation

--- scentree/io/__init__.py
+++ scentree/io/__init__.py
@@ -1,18 +1,20 @@
 from typing import List, Optional, TypedDict
 
 
-class MapColumns(TypedDict):
+class MapColumnsStages(TypedDict):
     """
     Mapping between a dataset and the ordering of its columns.
 
     Attributes:
         dataset (str): Name of the dataset.
         columns (List[int]): List of column indices defining their order.
+        stages (List[int]): List of column stages.
     """
 
     dataset: str
     columns: List[int]
+    stages: List[int]
 
 
 # Optional list of dataset-to-column mappings
-MapColsNames = Optional[List[MapColumns]]
+MapColsNamesStages = Optional[List[MapColumnsStages]]
